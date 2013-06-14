a6c27b9b69a855e906e71fd72021b085793598f8
pairliu <pairliu@gmail.com>
2013-06-14 22:25:48 +0800

    Initial DB design

--- src/main/resources/create_database.sql
+++ src/main/resources/create_database.sql
@@ -26,7 +26,7 @@ CREATE  TABLE users (
   name VARCHAR(45) NOT NULL ,
   email VARCHAR(45) ,
   cred_type VARCHAR(10) ,
-  cred_id VARCHAR(45) NOT NULL ,
+  cred_num VARCHAR(45) NOT NULL ,
   password VARCHAR(200) NOT NULL ,
   birth_date DATE ,
   address VARCHAR(500) ,
