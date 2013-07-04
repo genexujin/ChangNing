e71b270f1e8af4ee57712495c2aafed51b2fd1fa
James Liu <pairliu@gmail.com>
2013-07-04 10:50:25 +0800

    Add the missing file for SMS.

--- src/main/java/com/xiangyun/sms/SingletonSMSClient.java
+++ src/main/java/com/xiangyun/sms/SingletonSMSClient.java
@@ -1,5 +1,6 @@
 package com.xiangyun.sms;
 
+import java.util.Date;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
@@ -10,9 +11,6 @@ public class SingletonSMSClient {
     private static Client client = null;
     //开发环境账号
     public static String password="220235";
-    
-    //生产环境账号
-//    public static String password = "704168";
 
     private SingletonSMSClient() {
     }
@@ -41,15 +39,20 @@ public class SingletonSMSClient {
     }
 
     //执行一次，激活账号
-
     public static void main(String str[]) {
-//        Client theclient = SingletonSMSClient.getClient("3SDK-EMY-0130-OJXQS", "068075");
+        Client theclient = SingletonSMSClient.getClient("0SDK-EBB-0130-NEVNN", "222769");
+        //No need to register again, because we have already register it.
 //        int i = theclient.registEx(password);
 //        System.out.println("注册结果:" + i);
-//        //
-//        //        theclient.sendSMS(new String[] { "18621910893", "13817245519", "13524173173" },
-//        //                          "测试短信,发送时间： " + new Date() + ",请查看是否收到及时 【祥韵公司】", 1);
-//        System.err.println("ended!");
-//        theclient.registEx(password);
+
+        theclient.sendSMS(new String[] { "13524173173" },
+                          "测试短信,发送时间： " + new Date() + ",请查看是否收到及时 【祥韵公司】", 1);
+        try {
+			System.out.println("Available SMS: " +  Math.floor(theclient.getBalance() / 0.09));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.err.println("ended!");        
     }
 }
