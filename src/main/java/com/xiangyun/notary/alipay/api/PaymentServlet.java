cbeb027e798e6e57bc20f46d8cd27daac5406706
Jin <xxx>
2013-08-27 20:15:00 +0800

    添加了支付相关的代码

---
+++ src/main/java/com/xiangyun/notary/alipay/api/PaymentServlet.java
@@ -0,0 +1,112 @@
+package com.xiangyun.notary.alipay.api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PaymentServlet
+ */
+public class PaymentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PaymentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// 支付类型
+		String payment_type = "1";
+		// 必填，不能修改
+		// 服务器异步通知页面路径
+		String notify_url = "http://localhost:8080/create_direct_pay_by_user-JAVA-UTF-8/notify_url.jsp";
+		// 需http://格式的完整路径，不能加?id=123这类自定义参数
+
+		// 页面跳转同步通知页面路径
+		String return_url = "http://localhost:8080/create_direct_pay_by_user-JAVA-UTF-8/return_url.jsp";
+		// 需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
+
+		// 卖家支付宝帐户
+		String seller_email = new String(request
+				.getParameter("WIDseller_email").getBytes("ISO-8859-1"),
+				"UTF-8");
+		// 必填
+
+		// 商户订单号
+		String out_trade_no = new String(request
+				.getParameter("WIDout_trade_no").getBytes("ISO-8859-1"),
+				"UTF-8");
+		// 商户网站订单系统中唯一订单号，必填
+
+		// 订单名称
+		String subject = new String(request.getParameter("WIDsubject")
+				.getBytes("ISO-8859-1"), "UTF-8");
+		// 必填
+
+		// 付款金额
+		String total_fee = new String(request.getParameter("WIDtotal_fee")
+				.getBytes("ISO-8859-1"), "UTF-8");
+		// 必填
+
+		// 订单描述
+
+		String body = new String(request.getParameter("WIDbody").getBytes(
+				"ISO-8859-1"), "UTF-8");
+		// 商品展示地址
+		String show_url = new String(request.getParameter("WIDshow_url")
+				.getBytes("ISO-8859-1"), "UTF-8");
+		// 需以http://开头的完整路径，例如：http://www.xxx.com/myorder.html
+
+		// 防钓鱼时间戳
+		String anti_phishing_key = "";
+		// 若要使用请调用类文件submit中的query_timestamp函数
+
+		// 客户端的IP地址
+		String exter_invoke_ip = "";
+		// 非局域网的外网IP地址，如：221.0.0.1
+
+		// ////////////////////////////////////////////////////////////////////////////////
+
+		// 把请求参数打包成数组
+		Map<String, String> sParaTemp = new HashMap<String, String>();
+		sParaTemp.put("service", "create_direct_pay_by_user");
+		sParaTemp.put("partner", AlipayConfig.partner);
+		sParaTemp.put("_input_charset", AlipayConfig.input_charset);
+		sParaTemp.put("payment_type", payment_type);
+		sParaTemp.put("notify_url", notify_url);
+		sParaTemp.put("return_url", return_url);
+		sParaTemp.put("seller_email", seller_email);
+		sParaTemp.put("out_trade_no", out_trade_no);
+		sParaTemp.put("subject", subject);
+		sParaTemp.put("total_fee", total_fee);
+		sParaTemp.put("body", body);
+		sParaTemp.put("show_url", show_url);
+		sParaTemp.put("anti_phishing_key", anti_phishing_key);
+		sParaTemp.put("exter_invoke_ip", exter_invoke_ip);
+
+		// 建立请求
+		String sHtmlText = AlipaySubmit.buildRequest(sParaTemp, "get", "确认");
+		out.println(sHtmlText);
+	}
+
+}
