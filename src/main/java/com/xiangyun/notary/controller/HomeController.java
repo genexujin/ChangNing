c23fdc7a2b62d0b65634893bbd5d669b54dd7af8
Jin <xxx>
2013-09-30 16:55:17 +0800

    短信开通

--- src/main/java/com/xiangyun/notary/controller/HomeController.java
+++ src/main/java/com/xiangyun/notary/controller/HomeController.java
@@ -1,17 +1,37 @@
 package com.xiangyun.notary.controller;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.xiangyun.sms.SingletonSMSClient;
+
 @Controller
 public class HomeController {
-	
-	 @RequestMapping(value = "/home.do")
-	    public ModelAndView goToHome() {
-	    	ModelAndView mav = new ModelAndView("home");
-	    	mav.addObject("title", "网上办证");
-	    	return mav;
-	    }
+
+	@RequestMapping(value = "/home.do")
+	public ModelAndView goToHome() {
+		ModelAndView mav = new ModelAndView("home");
+		mav.addObject("title", "网上办证");
+		return mav;
+	}
+
+	@RequestMapping(value = "/smsRegister.do")
+	public void registerSMS(HttpServletResponse response) throws Exception {
+		String msg = SingletonSMSClient.register();
+		PrintWriter out = response.getWriter();
+		out.print(msg);
+	}
+
+	@RequestMapping(value = "/smsLogout.do")
+	public void logoutSMS(HttpServletResponse response) throws Exception {
+		String msg = SingletonSMSClient.logout();
+		PrintWriter out = response.getWriter();
+		out.print(msg);
+	}
 
 }
--- src/main/java/com/xiangyun/sms/SingletonSMSClient.java
+++ src/main/java/com/xiangyun/sms/SingletonSMSClient.java
@@ -8,51 +8,84 @@ import cn.emay.sdk.client.api.Client;
 
 public class SingletonSMSClient {
 
-    private static Client client = null;
-    //开发环境账号
-    public static String password="220235";
+	private static Client client = null;
+	// 开发环境账号
+	// public static String password="220235";
 
-    private SingletonSMSClient() {
-    }
+	// HJY环境账号
+	//public static String password = "704168";
 
-    public synchronized static Client getClient(String softwareSerialNo, String key) {
-        if (client == null) {
-            try {
-                client = new Client(softwareSerialNo, key);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return client;
-    }
+	// CNGZC环境账号
+	 public static String password = "885365";
 
-    public synchronized static Client getClient() {
-        ResourceBundle bundle = PropertyResourceBundle.getBundle("config");
-        if (client == null) {
-            try {
-                client = new Client(bundle.getString("softwareSerialNo"), bundle.getString("key"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return client;
-    }
+	private SingletonSMSClient() {
+	}
 
-    //执行一次，激活账号
-    public static void main(String str[]) {
-        Client theclient = SingletonSMSClient.getClient("0SDK-EBB-0130-NEVNN", "222769");
-        //No need to register again, because we have already register it.
-//        int i = theclient.registEx(password);
-//        System.out.println("注册结果:" + i);
+	public synchronized static Client getClient(String softwareSerialNo,
+			String key) {
+		if (client == null) {
+			try {
+				client = new Client(softwareSerialNo, key);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return client;
+	}
 
-//        theclient.sendSMS(new String[] { "18621910893" },
-//                          "测试短信,发送时间： " + new Date() + ",请查看是否收到及时 【祥韵公司】", 1);
-        try {
-			System.out.println("Available SMS: " +  Math.floor(theclient.getBalance() / 0.10));
+	/**
+	 * 注册
+	 * @return
+	 */
+	public static String register() {
+
+		int i = getClient().registEx(password);
+		System.out.println("注册结果:" + i);
+
+		return new Integer(i).toString();
+	}
+	
+	/**
+	 * 注销
+	 * @return
+	 */
+	public static String logout() {
+
+		int i = getClient().logout();
+		System.out.println("注册结果:" + i);
+
+		return new Integer(i).toString();
+	}
+
+	public synchronized static Client getClient() {
+		ResourceBundle bundle = PropertyResourceBundle.getBundle("config");
+		if (client == null) {
+			try {
+				client = new Client(bundle.getString("softwareSerialNo"),
+						bundle.getString("key"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return client;
+	}
+
+	// 执行一次，激活账号
+	public static void main(String str[]) {
+		Client theclient = SingletonSMSClient.getClient();
+		// No need to register again, because we have already register it.
+		// int i = theclient.registEx(password);
+		// System.out.println("注册结果:" + i);
+
+		// theclient.sendSMS(new String[] { "18621910893" },
+		// "测试短信,发送时间： " + new Date() + ",请查看是否收到及时 【祥韵公司】", 1);
+		try {
+			System.out.println("Available SMS: "
+					+ Math.floor(theclient.getBalance() / 0.09));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        System.err.println("ended!");        
-    }
+		System.err.println("ended!");
+	}
 }
